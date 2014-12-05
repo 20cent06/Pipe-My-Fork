ddd1166b5e79fc5743d09f44047c44ecff108443
Pacheco Mélanie <pacheco.melanie2a@gmail.com>
2014-12-05 02:19:30 +0100

    BDD COMPLET

--- BDD/pipe-my-fork.sql
+++ BDD/pipe-my-fork.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Jeu 04 Décembre 2014 à 22:20
+-- Généré le :  Ven 05 Décembre 2014 à 02:18
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -23,6 +23,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `foyer`
+--
+
+CREATE TABLE IF NOT EXISTS `foyer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` text COLLATE utf8_bin NOT NULL,
+  `nbPersonne` int(11) NOT NULL,
+  `lieux` varchar(45) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `lieux` (`lieux`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `localisation`
+--
+
+CREATE TABLE IF NOT EXISTS `localisation` (
+  `lieux` varchar(45) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`lieux`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `news`
 --
 
@@ -30,6 +56,7 @@ CREATE TABLE IF NOT EXISTS `news` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `titre` text COLLATE utf8_bin NOT NULL,
   `info` text COLLATE utf8_bin NOT NULL,
+  `image` text COLLATE utf8_bin,
   `dates` date NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=2 ;
@@ -38,8 +65,96 @@ CREATE TABLE IF NOT EXISTS `news` (
 -- Contenu de la table `news`
 --
 
-INSERT INTO `news` (`id`, `titre`, `info`, `dates`) VALUES
-(1, 'Ebola', 'Le virus ravage le pays...', '2014-12-03');
+INSERT INTO `news` (`id`, `titre`, `info`, `image`, `dates`) VALUES
+(1, 'Ebola', 'Le virus ravage le pays...', NULL, '2014-12-03');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `personne`
+--
+
+CREATE TABLE IF NOT EXISTS `personne` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` text COLLATE utf8_bin NOT NULL,
+  `prenom` text COLLATE utf8_bin NOT NULL,
+  `age` int(11) NOT NULL,
+  `sexe` text COLLATE utf8_bin NOT NULL,
+  `idFoyer` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idFoyer` (`idFoyer`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `persvacciner`
+--
+
+CREATE TABLE IF NOT EXISTS `persvacciner` (
+  `idPersonne` int(11) NOT NULL,
+  `RefVaccin` varchar(45) COLLATE utf8_bin NOT NULL,
+  `dates` date NOT NULL,
+  PRIMARY KEY (`idPersonne`,`RefVaccin`),
+  KEY `RefVaccin` (`RefVaccin`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `stock`
+--
+
+CREATE TABLE IF NOT EXISTS `stock` (
+  `RefVaccin` varchar(45) COLLATE utf8_bin NOT NULL,
+  `idFoyer` int(11) NOT NULL,
+  `quantiteVaccin` int(11) NOT NULL,
+  PRIMARY KEY (`RefVaccin`,`idFoyer`),
+  KEY `idFoyer` (`idFoyer`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `vaccination`
+--
+
+CREATE TABLE IF NOT EXISTS `vaccination` (
+  `RefVaccin` varchar(45) COLLATE utf8_bin NOT NULL,
+  `nomVaccin` text COLLATE utf8_bin NOT NULL,
+  `version` text COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`RefVaccin`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `foyer`
+--
+ALTER TABLE `foyer`
+  ADD CONSTRAINT `foyer_ibfk_1` FOREIGN KEY (`lieux`) REFERENCES `localisation` (`lieux`);
+
+--
+-- Contraintes pour la table `personne`
+--
+ALTER TABLE `personne`
+  ADD CONSTRAINT `personne_ibfk_1` FOREIGN KEY (`idFoyer`) REFERENCES `foyer` (`id`);
+
+--
+-- Contraintes pour la table `persvacciner`
+--
+ALTER TABLE `persvacciner`
+  ADD CONSTRAINT `persvacciner_ibfk_1` FOREIGN KEY (`idPersonne`) REFERENCES `personne` (`id`),
+  ADD CONSTRAINT `persvacciner_ibfk_2` FOREIGN KEY (`RefVaccin`) REFERENCES `vaccination` (`RefVaccin`);
+
+--
+-- Contraintes pour la table `stock`
+--
+ALTER TABLE `stock`
+  ADD CONSTRAINT `stock_ibfk_1` FOREIGN KEY (`RefVaccin`) REFERENCES `vaccination` (`RefVaccin`),
+  ADD CONSTRAINT `stock_ibfk_2` FOREIGN KEY (`idFoyer`) REFERENCES `foyer` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
