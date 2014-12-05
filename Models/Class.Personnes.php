4b8f529dda5158942c7ca57f3fe0f99ffdd87d99
Valentin PASSE <valentin-passe@hotmail.fr>
2014-12-05 06:05:39 +0100

    t

--- Models/Class.Personnes.php
+++ Models/Class.Personnes.php
@@ -2,44 +2,24 @@
 
 class Personnes{
 	private $id;   		
-  	private $titre;
-  	private $info;    		
-	private $date; /* contient tout les fichiers dans les différents groupes */
-	private $image;
+  	private $nom;
+  	private $prenom;    		
+	private $age;
+	private $sexe;
 /**
  * Constructeur privé, crée l'instance de l'utilisateur
  * pour toutes les méthodes de la classe
  */				
 	public function __construct($id){
 		$pdo = PdoTpPhp::getPdoTpPhp();
-		$personne = $pdo->getGroupe($idPersonne); 
+		$personne = $pdo->getPersonne($id); 
 
-		$this->utilisateur = $pdo->getUtilisateur($unIdUtil);
-
-		$this->idGroupe = $unGroupe['IDGROUPE'];
-		$this->nomGroupe = $unGroupe['NOMGROUPE'];
-		$this->chemin = $unGroupe['CHEMINGROUPE'];
-		$this->nbFichiers = $pdo->getNbFichiersGroupe($unIdGroupe);
+		$this->id = $personne['ID'];
+		$this->nom = $personne['NOM'];
+		$this->prenom = $personne['PRENOM'];
+		$this->age = $personne['AGE'];
+		$this->sexe = $personne['SEXE'];
 			
-		$lesFichiers = $pdo -> getFichiersGroupe($unIdGroupe);
-		if ($this->idGroupe == 5){
-
-			$salons = salon::getSalons($this->utilisateur['ID']);
-			foreach ($salons as $unSalon) {		
-				$leFichier = new fichier(6, $this->utilisateur['ID']);
-				$nomFichier = $leFichier->getNomFichier() . " - " . $unSalon['NOMSALON'];
-				$cheminFichier = $leFichier->getChemin() . $unSalon['CODESALON'] . ".PDF";
-				array_push($this->fichiers, array("IDFICHIER"=>6, "NOMFICHIER"=>$nomFichier, "CHEMINFICHIER"=>$cheminFichier ));
-			}
-
-		}
-
-		else{
-			foreach ($lesFichiers as $ligneFichier){
-				$leFichier = new fichier($ligneFichier['IDFICHIER'], $this->utilisateur['ID']);
-				array_push($this->fichiers, array("IDFICHIER"=>$leFichier->getId(), "NOMFICHIER"=>$leFichier->getNomFichier(), "CHEMINFICHIER"=>$leFichier->getChemin() ));
-			}			
-		}
 
 	} // public function __construct(...)
 
--- Models/pdo.php
+++ Models/pdo.php
@@ -69,3 +69,176 @@ class PdoTpPhp{
 
 }
 
+/*
+
+	public function getInfosAnimateur($login, $mdp){
+		$req = "SELECT ID, LOGIN, PRIVILEGE, NOMCOMPLET FROM UTILISATEUR 
+		where LOGIN='$login' and PASSWORD='$mdp'";
+		$rs = PdoTpPhp::$monPdo->query($req);
+		$ligne = $rs->fetch();
+		return $ligne;
+	}
+	public function setPasswordUtilisateur($id,$nouveauMotDePasse){
+
+		$req = "UPDATE UTILISATEUR SET PASSWORD ='".$nouveauMotDePasse."' WHERE id=".$id;
+		$rs = PdoTpPhp::$monPdo->query($req);
+	}
+	public function getAnimateurs(){
+		$req = "SELECT ID, PRIVILEGE, NOMCOMPLET FROM UTILISATEUR WHERE PRIVILEGE =1 AND ID !=".$_SESSION['ID']." ORDER BY NOMCOMPLET";
+		$rs = PdoTpPhp::$monPdo->query($req);
+		$tableau = $rs->fetchAll();
+		return $tableau;
+	}
+	public function getUtilisateurs(){
+		$req = "SELECT ID, PRIVILEGE, NOMCOMPLET FROM UTILISATEUR WHERE PRIVILEGE =2 ORDER BY NOMCOMPLET" ;
+		$rs = PdoTpPhp::$monPdo->query($req);
+		$tableau = $rs->fetchAll();
+		return $tableau;
+	}
+	public function getAdministrateurs(){
+		$req = "SELECT ID, PRIVILEGE, NOMCOMPLET FROM UTILISATEUR WHERE PRIVILEGE =3 AND ID !=".$_SESSION['ID']." ORDER BY NOMCOMPLET";
+		$rs = PdoTpPhp::$monPdo->query($req);
+		$tableau = $rs->fetchAll();
+		return $tableau;
+	}
+	public function getUtilisateur($idUtilisateur){
+		$req = "SELECT ID, LOGIN, PRIVILEGE, NOMCOMPLET FROM UTILISATEUR WHERE ID = '$idUtilisateur' ";
+		$rs = PdoTpPhp::$monPdo->query($req);
+		$ligne = $rs->fetch();
+		return $ligne;
+	}
+	public function getFichier($idFichier){
+		$req = "SELECT IDFICHIER, NOMFICHIER, CHEMINFICHIER, IDGROUPE FROM FICHIER WHERE IDFICHIER='$idFichier'";
+		$rs = PdoTpPhp::$monPdo->query($req);
+		$ligne = $rs->fetch();
+		return $ligne;
+	}
+	public function getDernFichier(){
+		$req = "SELECT COUNT(*) FROM FICHIER";
+		$rs = PdoTpPhp::$monPdo->query($req);
+		$ligne = $rs->fetch();
+		$nb = $ligne[0] - 1;
+		return $nb;
+	}
+	public function getFichiersGroupe($idGroupe){
+		$req = "SELECT IDFICHIER, NOMFICHIER, CHEMINFICHIER FROM FICHIER WHERE IDGROUPE='$idGroupe'";
+		$rs = PdoTpPhp::$monPdo->query($req);
+		$ligne = $rs->fetchAll();
+		return $ligne;
+	}
+	public function getNbFichiersGroupe($idGroupe){
+		$req = "SELECT COUNT(*) FROM FICHIER WHERE IDGROUPE =".$idGroupe;
+		$rs = PdoTpPhp::$monPdo->query($req);
+		$ligne = $rs->fetch();
+		$nb = $ligne[0];
+		return $nb;
+	}
+	public function getGroupe($idGroupe){
+		$req = "SELECT IDGROUPE, NOMGROUPE, CHEMINGROUPE FROM GROUPEFICHIER
+		WHERE IDGROUPE ='$idGroupe'";
+		$rs = PdoTpPhp::$monPdo->query($req);
+		$ligne = $rs->fetch();
+		return $ligne;
+	}
+	public function getGroupes(){
+		$req = "SELECT IDGROUPE, NOMGROUPE, CHEMINGROUPE FROM GROUPEFICHIER";
+		$rs = PdoTpPhp::$monPdo->query($req);
+		$tableau = $rs->fetchAll();
+		return $tableau;
+	}
+	public function getSalon($id){ 
+		$req = "SELECT IDSALON,CODESALON, NOMSALON, ID FROM SALON WHERE IDSALON='$id'";
+		$rs = PdoTpPhp::$monPdo->query($req);
+		$ligne = $rs->fetch();
+		return $ligne;
+	}
+	public function getSalonsUtilisateur($idUtil){
+		$req = "SELECT IDSALON,CODESALON, NOMSALON, ID FROM SALON WHERE ID='$idUtil'";
+		$rs = PdoTpPhp::$monPdo->query($req);
+		$tableau = $rs->fetchAll();
+		return $tableau;
+	}
+	public function getToutSalons(){
+		$req = "SELECT IDSALON,CODESALON, NOMSALON, ID FROM SALON ORDER BY CODESALON";
+		$rs = PdoTpPhp::$monPdo->query($req);
+		$tableau = $rs->fetchAll();
+		return $tableau;
+	}
+	public function getDossier($idDossier){
+		$req = "SELECT IDDOSSIER, NOMDOSSIER FROM DOSSIER
+		WHERE IDDOSSIER ='$idDossier'";
+		$rs = PdoTpPhp::$monPdo->query($req);
+		$ligne = $rs->fetch();
+		return $ligne;
+	}
+	public function getUnHistorique($type,$action,$tri,$ordre,$debut,$fin){
+		$req = "SELECT idEntree, (SELECT COUNT(*) FROM HISTORIQUE WHERE TYPE =  '".$type."' AND ACTION='".$action."') as NBLIGNES, TYPE , ACTION , DETAIL, DATE, UTILISATEUR
+		FROM HISTORIQUE WHERE TYPE =  '".$type."' AND ACTION='".$action."' ORDER BY ".$tri." ".$ordre. " LIMIT ". $debut." , ".$fin;
+		$rs = PdoTpPhp::$monPdo->query($req);
+		$tableau = $rs->fetchAll();
+		return $tableau;
+	}
+	public function getDossiers(){
+		$req = "SELECT IDDOSSIER, NOMDOSSIER FROM DOSSIER";
+		$rs = PdoTpPhp::$monPdo->query($req);
+		$tableau = $rs->fetchAll();
+		return $tableau;
+	}
+	public function getfichierParChemin($chemingroupe,$cheminFichier){
+		$req = "select * from FICHIER f, GROUPEFICHIER g where f.idgroupe=g.idgroupe
+			and chemingroupe ='".$chemingroupe."' and cheminFichier='".$cheminFichier."'";
+		$rs = PdoTpPhp::$monPdo->query($req);
+		$tableau = $rs->fetchAll();
+		return $tableau;
+	}
+	public function supprimerFichier($idFichier){
+		$req = 'delete from FICHIER where idfichier='.$idFichier;
+		$rs = PdoTpPhp::$monPdo->query($req);
+	}
+	public function ajouterUtilisateur($login, $password,$privilege,$nomComplet){
+		$req = "INSERT INTO UTILISATEUR(login,password,privilege,nomComplet) VALUES ('".$login."','".$password."','".$privilege."','".$nomComplet."')";
+		$rs = PdoTpPhp::$monPdo->query($req);
+	}
+public function creerEntreeHistorique($type, $action,$detail,$nomUtilisateur){
+		$req = "INSERT INTO HISTORIQUE(Type, Action, Detail, Date, Utilisateur)
+		 VALUES ('".$type."','".$action."','".$detail."',Now(),'".$nomUtilisateur."')";
+		$rs = PdoTpPhp::$monPdo->query($req);
+	}
+	public function editerUtilisateur($id,$login, $password,$privilege,$nomComplet){
+		if ($password != '') {
+			$password=",password='".$password."'";
+		}
+		$req = "UPDATE UTILISATEUR SET login='".$login."'".$password.",privilege='".$privilege."',nomComplet='".$nomComplet."' WHERE ID=".$id;
+		$rs = PdoTpPhp::$monPdo->query($req);
+	}
+	public function supprimerUtilisateur($id){
+		$req = 'delete from UTILISATEUR where id='.$id;
+		$rs = PdoTpPhp::$monPdo->query($req);
+	}
+	public function ajouterSalon($codeSalon,$nomSalon,$idUtilisateur)
+	{
+		$req = "INSERT INTO SALON(CODESALON,NOMSALON,ID) VALUES ('".$codeSalon."','".$nomSalon."','".$idUtilisateur."')";
+		$rs = PdoTpPhp::$monPdo->query($req);
+	}
+	public function editerSalon($idSalon,$codeSalon,$nomSalon,$idUtilisateur){
+		$req = "UPDATE SALON SET CODESALON='".$codeSalon."', nomSalon='".$nomSalon."',id=".$idUtilisateur." WHERE IDSALON=".$idSalon;
+		$rs = PdoTpPhp::$monPdo->query($req);
+	}
+	public function supprimerSalon($idSalon){
+		$req = 'delete from SALON where idSalon='.$idSalon;
+		$rs = PdoTpPhp::$monPdo->query($req);
+	}
+	public function setNomFichier($idFichier, $nouveauNom){
+		$req = "UPDATE FICHIER SET nomFichier = '".$nouveauNom."' WHERE idFichier = ".$idFichier;
+		$rs = PdoTpPhp::$monPdo->query($req);
+		}
+	public function newFichier($nomFichier, $cheminFichier, $idGroupe){
+		$req = "INSERT INTO FICHIER (nomFichier, cheminFichier, idGroupe) VALUES ('" . $nomFichier . "', '" . $cheminFichier . "', " . $idGroupe . ");";
+		$rs = PdoTpPhp::$monPdo->prepare($req);
+		$rs->execute(); 
+		$idFic=PdoTpPhp::$monPdo->lastInsertId(); 
+		return $idFic;
+		}
+	}ob_end_clean();
+
+	*/
